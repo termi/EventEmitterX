3ae91bc984eed1ad0ab71053c88fc08ab7e7b3f4
Egor Halimonenko <termi_uc@inbox.ru>
2025-12-18 01:02:17 +0300

    feat(demo): feat(tests): update eventSignals-test-app: rewrite first demo page with Counters

--- demo/eventSignals-test-app/pages/1.one.metadata.ts
+++ demo/eventSignals-test-app/pages/1.one.metadata.ts
@@ -4,8 +4,15 @@ import { i18nString$$ } from "../state/i18n";
 
 const menuItemTitle = 'Счетчики';
 const menuItemTitle$ = i18nString$$(menuItemTitle);
+const unicodeIcon = '🖩';
+// './static/calculator.svg'
+const staticIconSrc = 'calculator.svg';
+// todo: const fontIcon = 'x';
+// todo: const iconsFontName = 'FontAwesome';
 
 export {
     menuItemTitle,
     menuItemTitle$,
+    unicodeIcon,
+    staticIconSrc,
 };
--- demo/eventSignals-test-app/pages/1.one.tsx
+++ demo/eventSignals-test-app/pages/1.one.tsx
@@ -5,25 +5,64 @@ import * as React from "react";
 import Counter from "../modules/Counter";
 
 import { mainState } from "../state/AppStates";
+import { i18nString$$ } from "../state/i18n";
 
-import { menuItemTitle$ } from './1.one.metadata';
+import css from "./1.one.module.css";
+
+const counterClasses: Parameters<typeof Counter>[0]["classes"] = {
+    className: css.counter,
+    title: css.counterTitle,
+    value: css.counterValue,
+    buttons: css.counterButtons,
+};
 
 export default function PageOne() {
     console.log('render PageOne');
 
     return (<>
-        <div style={{ display: 'flex' }}>
-            <mainState.computed1$.component sFC={Counter} title="Counter1"/>
-            <Counter eventSignal={mainState.computed2$} title="Counter2"/>
-            {mainState.computed2$}
+        <div className={css.pageOne}>
+            <div className={css.countersContainer}>
+                <div className={css.sectionsContainer}>
+                    <div className={css.countersSection}>
+                        <h2 className={css.sectionTitle}>{i18nString$$('Управление счетчиками')}</h2>
+
+                        <mainState.computed1$.component sFC={Counter} classes={counterClasses}/>
+
+                        <Counter eventSignal={mainState.computed2$} classes={counterClasses}/>
+
+                        {mainState.computed2$}
+                    </div>
+
+                    <div className={css.summarySection}>
+                        <h2 className={css.sectionTitle}>{i18nString$$('Текущие значения')}</h2>
+
+                        <div className={css.summaryDisplay}>
+                            <div className={css.summaryItem}>
+                                <div className={css.summaryLabel}>{mainState.counter1$.data.title}</div>
+                                <div className={css.summaryValue}>{mainState.counter1$}</div>
+                            </div>
+
+                            <div className={css.summaryItem}>
+                                <div className={css.summaryLabel}>{mainState.counter2$.data.title}</div>
+                                <div className={css.summaryValue}>{mainState.counter2$}</div>
+                            </div>
+
+                            <div className={`${css.summaryItem} ${css.totalDisplay}`}>
+                                <div className={css.summaryLabel}>{mainState.countersSum$.data.title}</div>
+                                <div className={css.summaryValue}>{mainState.countersSum$}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            {mainState.jsonPlaceholderUser1$}
+            <br/>
+            <button onClick={mainState.jsonPlaceholderUser1$.data.getNextUser}>
+                {mainState.jsonPlaceholderUser1$.data.getNextUser.title}
+            </button>
         </div>
-        <br />
-        {mainState.jsonPlaceholderUser1$}
-        <br />
-        <button onClick={() => {
-            return mainState.jsonPlaceholderUser1$.set((_, currentUserId) => {
-                return ++currentUserId;
-            });
-        }}>get next user</button>
     </>);
 }
+
+Object.freeze(Object.setPrototypeOf(counterClasses, null));
