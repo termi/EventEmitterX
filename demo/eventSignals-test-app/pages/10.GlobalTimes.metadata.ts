4cfe0755e263e50ad1355ba8ba892db3343a242e
Egor Halimonenko <termi_uc@inbox.ru>
2025-12-18 01:00:50 +0300

    feat(demo): ref(view): update eventSignals-test-app: GlobalTimes page: refactoring and add table view

--- demo/eventSignals-test-app/pages/10.GlobalTimes.metadata.ts
+++ demo/eventSignals-test-app/pages/10.GlobalTimes.metadata.ts
@@ -4,8 +4,13 @@ import { i18nString$$ } from "../state/i18n";
 
 const menuItemTitle = 'Мировое время';
 const menuItemTitle$ = i18nString$$(menuItemTitle);
+const unicodeIcon = '🌍';
+// './static/calculator.svg'
+const staticIconSrc = 'calculator.svg';
 
 export {
     menuItemTitle,
     menuItemTitle$,
+    unicodeIcon,
+    staticIconSrc,
 };
