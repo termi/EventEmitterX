217493d5bd40f9a1368583fa304a49d9deccdb09
Egor Halimonenko <termi_uc@inbox.ru>
2026-01-09 14:12:56 +0300

    dev(demo): ref(ErrorBoundary): update errors handling in React components using ErrorBoundary

--- demo/eventSignals-test-app/$components/ErrorView.tsx
+++ demo/eventSignals-test-app/$components/ErrorView.tsx
@@ -4,17 +4,25 @@ import * as React from "react";
 
 import type { EventSignal } from '~/modules/EventEmitterEx/EventSignal';
 
-export default function ErrorView({ eventSignal, children }: { eventSignal: EventSignal<any, any, any>, children: React.ReactNode }) {
+export default function ErrorView({ eventSignal, children }: { eventSignal: EventSignal<any, any, any>, children?: React.ReactNode }) {
     const { lastError } = eventSignal;
 
     if (!lastError) {
         return null;
     }
 
-    const hint = (lastError instanceof Error) ? lastError.stack : void 0;
+    return ErrorViewSimple({ error: lastError, children });
+}
+
+export function ErrorViewSimple({ error, children }: { error?: Error | number | string, children?: React.ReactNode, resetErrorBoundary: (...args: any[]) => void }) {
+    if (!error) {
+        return null;
+    }
+
+    const hint = (error instanceof Error) ? error.stack : void 0;
 
     return (<span className="ErrorView" style={{ color: 'red' }} title={hint}>
-        <span className="ErrorView__error">⚠️{String(lastError["message"] || lastError)}</span>
+        <span className="ErrorView__error">⚠️{String(error["message"] || error)}</span>
         {children ? (<span className="ErrorView__children">️{children}</span>) : null}
     </span>);
 }
